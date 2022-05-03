b218e88d986ee5c1e79e5472581082ec6d788502
Nick <nickhowlett13@gmail.com>
2022-05-03 23:31:15 +0800

    siglogcfg updates

--- siglogConfig.py
+++ siglogConfig.py
@@ -7,6 +7,7 @@ path = r"C:\Users\nickk\Documents\Code\Python\work\Signal-log-reader\Data\SIGLOG
 class SiglogConfig:
     def __init__(self, siglogcfg_path):
         self.sigLog_cfg = siglogcfg_path
+        # I don't think this be here.the file need to be checked before the class is made
         try:
             with open(self.sigLog_cfg, 'r') as raw_d:
                 self.raw_data = raw_d.readlines()
